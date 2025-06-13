7321797b06d03a51f4c20eb949a27949777dd8df
Matheuss58 <matheus.galvao.santos@escola.pr.gov.br>
2025-06-13 20:44:51 -0300

    sétimo update

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'musicas-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/musicas/',
+  '/musicas/covers/'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(response => response || fetch(e.request))
+  );
+});
